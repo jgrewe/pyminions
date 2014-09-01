7b6ec966f2b415735248ed91f8859b40cfeaa952
Fabian Sinz <fabian.sinz@uni-tuebingen.de>
2014-09-01 12:04:36 +0200

    added cc license and changed to read the docs theme

--- README.md
+++ README.md
@@ -5,4 +5,7 @@ Collection of useful helper functions missing in other libraries.
 
 pyminions is written to be compatible with Python 3.x.
 
-The code is published under the creative commons license (http://creativecommons.org/licenses/by-sa/3.0/)
+The code is published under the creative commons license (http://creativecommons.org/licenses/by-sa/3.0/).
+
+The documentation requires the sphinx_rtd_theme package which can be installed via pip:
+>>> pip install sphinx_rtd_theme
--- doc/source/conf.py
+++ doc/source/conf.py
@@ -15,6 +15,7 @@
 
 import sys
 import os
+import sphinx_rtd_theme
 
 # If extensions (or modules to document with autodoc) are in another directory,
 # add these directories to sys.path here. If the directory is relative to the
@@ -55,7 +56,7 @@ master_doc = 'index'
 
 # General information about the project.
 project = 'pyminions - a python helper function collection'
-copyright = '2014, Fabian Sinz, Jan Grewe'
+copyright = '2014, Fabian Sinz, Jan Grewe, creative commons license (http://creativecommons.org/licenses/by-sa/3.0/)'
 
 # The version info for the project you're documenting, acts as replacement for
 # |version| and |release|, also used in various other places throughout the
@@ -109,12 +110,15 @@ pygments_style = 'sphinx'
 
 # The theme to use for HTML and HTML Help pages.  See the documentation for
 # a list of builtin themes.
-html_theme = 'default'
+# html_theme = 'default'
+html_theme = "sphinx_rtd_theme"
+
+html_theme_path = [sphinx_rtd_theme.get_html_theme_path()]
 
 # Theme options are theme-specific and customize the look and feel of a theme
 # further.  For a list of options available for each theme, see the
 # documentation.
-#html_theme_options = {}
+html_theme_options = {}
 
 # Add any paths that contain custom themes here, relative to this directory.
 #html_theme_path = []
