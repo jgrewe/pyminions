d182cca385202e4c6e62c7c06dd1da3d81b005bb
Fabian Sinz <fabian.sinz@uni-tuebingen.de>
2014-09-09 17:06:57 +0200

    violinplot but without comments

--- plotting/plots.py
+++ plotting/plots.py
@@ -39,3 +39,28 @@ def hinton(ax, mu, sigma, mu_max=None, sigma_max=None, cmap=None):
     ax.autoscale_view()
     ax.invert_yaxis()
     return ret
+
+def violinplot_smooth(ax, x, Y, tau, delta=.5, label=None, y_range=None, **kwargs):
+    if type(x) != np.ndarray:
+        x = np.atleast_1d(x)
+    x = x.ravel()
+    if type(Y) != np.ndarray:
+        Y = np.atleast_2d(np.asarray(Y))
+    if Y.shape[0] == 1:
+        Y = Y.T
+    if y_range is None:
+        t = np.arange(np.amin(Y) - 3 * tau, np.amax(Y) + 3 * tau, tau / 10.)
+    else:
+        t = np.arange(y_range[0], y_range[1], tau / 10.)
+    col = np.linspace(0, 1, len(t))
+    c = np.r_[t, t[::-1]]
+    cc = np.r_[col, col[::-1]]
+    for j in xrange(Y.shape[1]):
+        n = reduce(lambda a, b: a + b, [np.exp(-.5 * (t - yt) ** 2. / tau ** 2) for yt in Y[:, j]])
+        n = n / np.amax(n) * delta
+        y = x[j] + np.r_[n, -n[::-1]]
+
+        ax.fill(y, c, label=label if j == 0 else None, **kwargs)
+        ax.plot(y, c, '-k', lw=.5)
+
+        ax.plot(0 * Y[:, j] + x[j], Y[:, j], 'ok', mfc='lightgray', markersize=2, lw=0)
