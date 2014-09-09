2fdf3f1f8a602828a0a05dbd8ba3467d1e22e03e
Jan Grewe <jan.grewe@g-node.org>
2014-09-09 14:53:21 +0200

    add method to estimate serial correlations of spike trains

--- spikes/spiketimes.py
+++ spikes/spiketimes.py
@@ -1,4 +1,9 @@
 import numpy as np
+try:
+    __import__('odml')
+except ImportError:
+    warnings.warn("odml package not available. Install via")
+
 
 def spike_times_to_binary(spike_times, sample_rate, duration):
     """
@@ -18,3 +23,33 @@ def spike_times_to_binary(spike_times, sample_rate, duration):
     indices = np.asarray(spike_times  * sample_rate, dtype=int)
     binary[indices] = 1
     return binary
+
+
+def serial_correlation(spike_times, max_lags=50, return_metadata=True):
+    """
+        Calculate the serial correlation for the the spike train provided by spike_times.
+
+    :param spike_times: the spike times of a single trial. This should be a 1D array.
+    :param max_lags: The number of lags to take into account
+    :param return_metadata: If true the analysis metadata are returned
+    :return: the serial correlation as a function of the lag, and, if wanted, the metadata.
+
+    """
+    if len(spike_times.shape) > 1:
+        raise ValueError("spike times must not be more than 1D.")
+
+    isis = np.diff(spike_times)
+    unbiased = isis - np.mean(isis, 0)
+    norm = np.sum(unbiased**2)
+    a_corr = np.correlate(unbiased, unbiased, "same")/norm
+    a_corr = a_corr[len(a_corr)/2:]
+
+    if not return_metadata or 'odml' not in globals():
+        return a_corr[:max_lags]
+
+    metadata = odml.Section("Serial correlation", "analysis/serial_correlation")
+    metadata.append(odml.Property("Description", "The serial correlation of inter-spike-intervals."))
+    metadata.append(odml.Property("NoOfLags", max_lags))
+    metadata.append(odml.Property("FirstLagCorrelation", a_corr[1]))
+    metadata.append(odml.Property("Date", dt.date.today().isoformat()))
+    return a_corr[:max_lags], metadata
